6d19bad3fa8063be3b7e113efa6f9b4bcbb24c36
Pranav Sunil Suryawanshi <pranavsur@cybage.com>
2023-03-10 19:17:35 +0530

    added exit
    Signed-off-by: Pranav Sunil Suryawanshi <pranavsur@cybage.com>

--- JenkinsDemo1/src/main/java/com/jenkins/demo1/JenkinsDemo1Application.java
+++ JenkinsDemo1/src/main/java/com/jenkins/demo1/JenkinsDemo1Application.java
@@ -1,15 +1,22 @@
 package com.jenkins.demo1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages="com.*")
 public class JenkinsDemo1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsDemo1Application.class, args);
+	@Autowired
+	ApplicationContext context;
+	
+	public static void main(String[] args) throws InterruptedException {
+		ConfigurableApplicationContext context = SpringApplication.run(JenkinsDemo1Application.class, args);
 		System.out.println("hi");
-	    
+	    Thread.sleep(5000);
+	    context.close();
 	}
 
 }
